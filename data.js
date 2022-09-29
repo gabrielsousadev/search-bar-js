a1616f216d370744c2ba0a1adf869e5aa4aadbd4
Recriando Games <gabriel.mattos17@hotmail.com>
2022-09-29 13:36:49 -0300

    Update

--- data.js
+++ data.js
@@ -3,18 +3,25 @@ const userCardContainer = document.querySelector("[data-user-cards-container]");
 const searchInput = document.querySelector("[data-search]");
 let users = [];
 
-searchInput.addEventListener("input", e => {
-    const value = e.target.value.toLowerCase()
-    users.forEach(user => {
-        const isVisible = user.name.toLowerCase().includes(value) ||
-                        user.email.toLowerCase().includes(value);
-        user.element.classList.toggle("hide", !isVisible);
+function searchBar() 
+{
+    searchInput.addEventListener("input", e => {
+        const value = e.target.value.toLowerCase()
+        users.forEach(user => {
+            const isVisible = user.name.toLowerCase().includes(value) ||
+                            user.email.toLowerCase().includes(value);
+            user.element.classList.toggle("hide", !isVisible);
+        });
+    
+        console.log(users)
     });
+}
 
-    console.log(users)
-});
+searchBar();
 
-fetch("https://jsonplaceholder.typicode.com/users")
+function data()
+{
+    fetch("https://jsonplaceholder.typicode.com/users")
     .then(res => res.json())
     .then(data => {
         users = data.map(user => {
@@ -27,4 +34,7 @@ fetch("https://jsonplaceholder.typicode.com/users")
             //console.log(user);
             return {name: user.name,email: user.email, element: card }
         });
-    });
+    });   
+}
+
+data();
